2c608cddbd33455d229f0ac82190783d039a5f55
ankit <a.baliyan008@gmail.com>
2024-01-01 13:45:38 +0530

    rearrange

--- bimamitra/wsgi.py
+++ bimamitra/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'bimamitra.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'bimamitra.settings'
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bimamitra.settings')
 
 application = get_wsgi_application()
